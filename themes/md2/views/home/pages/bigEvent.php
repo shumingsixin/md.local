2f2678cd93fba286046ddaa500cd1fd3131aab1c
liukui <kui.liu@mingyizhudao.com>
2016-04-25 17:21:39 +0800

    大事记

---
+++ themes/md2/views/home/pages/bigEvent.php
@@ -0,0 +1,99 @@
+<?php ?>
+<?php
+$urlResImage = Yii::app()->theme->baseUrl . "/images/";
+$this->show_footer = false;
+$this->show_header = false;
+?>
+<style>
+    section.active{top:0px;}
+
+    #bigEvent_article{
+        background-color: #F0EFF4;
+    }
+    #bigEvent_article .january{
+        background: url('<?php echo $urlResImage; ?>/event/bigEvent/january.png') no-repeat;
+        background-size: 14px;
+    }
+    #bigEvent_article .februaryStepOne{
+        background: url('<?php echo $urlResImage; ?>/event/bigEvent/februaryStepOne.png') no-repeat;
+        background-size: 14px;
+    }
+    #bigEvent_article .februaryStepTwo{
+        background: url('<?php echo $urlResImage; ?>/event/bigEvent/februaryStepTwo.png') no-repeat;
+        background-size: 14px;
+    }
+    #bigEvent_article .februaryStepThree{
+        background: url('<?php echo $urlResImage; ?>/event/bigEvent/februaryStepThree.png') no-repeat;
+        background-size: 14px;
+    }
+    #bigEvent_article .march{
+        background: url('<?php echo $urlResImage; ?>/event/bigEvent/march.png') no-repeat;
+        background-size: 14px;
+    }
+</style>
+<div id="section_container" <?php echo $this->createPageAttributes(); ?> >
+    <section id="" class="active" data-init="true">
+        <article id="bigEvent_article" class="active" data-scroll="true">
+            <div class="pt10 pl10 pr10 text-justify">
+                <div class="br5 bg-white pad5">
+                    继2015年12月名医主刀入选“第十届中国最具投资价值企业50强”后，名医主刀在2016年第一季度再获多项殊荣，让我们一起来回顾下
+                </div>
+                <div class="pt20 color-blue3">
+                    2016
+                </div>
+                <div class="font-s12 color-gray7">
+                    <div class="pt10 grid">
+                        <div class="col-0 january w20p">
+                        </div>
+                        <div class="col-1 pb5">
+                            <div class="pb5 color-black font-14">1月</div>
+                            <div class="br5 bg-white pad5">
+                                名医主刀荣获亿欧网举办的“2015年度中国互联网+最具创新力”奖。入围项目的创新不仅是模式创新、服务业务创新，还包括技术创新、体制机制创新和管理创新等，且主要根据全社会公开项目征集、亿欧网评测和参考第三方数据（艾瑞、Testin云测、TakingData）得出。
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pt3 grid">
+                        <div class="col-0 februaryStepOne w20p">
+                        </div>
+                        <div class="col-1 pb5">
+                            <div class="pt13 pb5 color-black font-14">2月</div>
+                            <div class="br5 bg-white pad5">
+                                名医主刀创始人、CEO苏舒入选福布斯“30位30岁以下亚洲年轻领袖榜（30 under 30）”。
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pt3 grid">
+                        <div class="col-0 februaryStepTwo w20p">
+                        </div>
+                        <div class="col-1 pt35 pb5">
+                            <div class="br5 bg-white pad5">国家信息中心信息化研究部、中国互联网协会分享经济工作委员会联合发布《中国分享经济发展报告2016》。名医主刀作为唯一一家移动医疗创业公司入选。名医主刀在配置闲置床位和医生碎片时间的过程中，践行着分享经济理念。
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pt3 grid">
+                        <div class="col-0 februaryStepThree w20p">
+                        </div>
+                        <div class="col-1 pt35 pb5">
+                            <div class="br5 bg-white pad5">
+                                动脉网发布2014-2015全球互联网医疗投融百强榜单，名医主刀榜上有名，也是唯一一个专精于手术领域的互联网医疗公司。
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pt3 grid">
+                        <div class="col-0 march w20p">
+                        </div>
+                        <div class="col-1 pb30">
+                            <div class="pt13 pb5 color-black font-14">3月</div>
+                            <div class="br5 bg-white pad5">
+                                在由辉瑞中国、中国平安、腾讯、君联资本四大行业代表企业跨界联手主办的医疗合伙人创新合作方案大奖赛上，名医主刀力拔头筹，荣获服务类冠军。
+                            </div>
+                            <div class="br5 bg-white pad5 mt10 color-black">
+                                感谢每一位名医的加入和支持，让我们一起再接再厉，让更多的患者在名医主刀好看病、看好病！
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </article>
+    </section>
+</div>
