24e8df597efd4fe534ba2846eae44c4173d08939
shumingsixin <shuming_sixin@163.com>
2016-05-09 17:52:55 +0800

    加密类更新

--- protected/models/core/CoreRasConfig.php
+++ protected/models/core/CoreRasConfig.php
@@ -5,8 +5,12 @@ class CoreRasConfig extends EActiveRecord {
     /**
      * @return string the associated database table name
      */
+    public function getDbConnection() {
+        return Yii::app()->db2;
+    }
+
     public function tableName() {
-        return 'core_rsa_config';
+        return 'encryption';
     }
 
     public static function model($className = __CLASS__) {
@@ -16,6 +20,7 @@ class CoreRasConfig extends EActiveRecord {
     public function attributeLabels() {
         return array(
             'id' => 'ID',
+            'key_name' => 'KEY名称',
             'client' => '使用来源客户端',
             'public_key' => '公钥',
             'private_key' => '私钥',
